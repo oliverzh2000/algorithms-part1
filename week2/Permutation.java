6ef5b9496d3dbc6fca603b0ff303861425acfe50
Oliver Zhang <oliverzh2000@gmail.com>
2018-07-03 08:54:19 -0700

    Finish week 2

---
+++ week2/Permutation.java
@@ -0,0 +1,15 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+    public static void main(String[] args) {
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+        int k = Integer.parseInt(args[0]);
+        for (int i = 0; i < k; i++) {
+            randomizedQueue.enqueue(StdIn.readString());
+        }
+        for (String string : randomizedQueue) {
+            StdOut.println(string);
+        }
+    }
+}
