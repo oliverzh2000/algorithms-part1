ff23f23a7a0d32ab49e06d0030484eb07999822c
Oliver Zhang <oliverzh2000@gmail.com>
2019-07-07 17:04:05 -0400

    Add MST API stub

---
+++ part2/week2/part2.week1.lesson/MST.java
@@ -0,0 +1,15 @@
+package part2.week1.lesson;
+
+public class MST {
+    public MST(EdgeWeightedGraph G) {
+
+    }
+
+    Iterable<Edge> edges() {
+        return null;
+    }
+
+    double weight() {
+        return 0;
+    }
+}
