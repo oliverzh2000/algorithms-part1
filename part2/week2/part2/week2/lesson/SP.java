54ecb7c8c883857f7ae7c875db0355487b1b788c
Oliver Zhang <oliverzh2000@gmail.com>
2019-07-09 21:57:23 -0400

    Add SP (shortest path single source) stub

---
+++ part2/week2/part2/week2/lesson/SP.java
@@ -0,0 +1,21 @@
+package part2.week2.lesson;
+
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+
+public class SP {
+    public SP(EdgeWeightedDigraph graph, int source) {
+
+    }
+
+    double distTo(int vertex) {
+        return -1;
+    }
+
+    Iterable<DirectedEdge> pathTo(int vertex) {
+        return null;
+    }
+
+    boolean hasPathTo(int vertex) {
+        return false;
+    }
+}
