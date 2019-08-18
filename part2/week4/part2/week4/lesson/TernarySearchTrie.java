c3e2358f01ed38b3e6632c40881bb7bb5a7646ad
Oliver Zhang <oliverzh2000@gmail.com>
2019-08-18 16:41:41 -0400

    Add get methods to TernarySearchTree

--- part2/week4/part2/week4/lesson/TernarySearchTrie.java
+++ part2/week4/part2/week4/lesson/TernarySearchTrie.java
@@ -1,6 +1,6 @@
 package part2.week4.lesson;
 
-public class TernarySearchTree<T> {
+public class TernarySearchTrie<T> {
     private static final int R = 256; // extended ASCII
     private Node root = new Node();
 
@@ -30,5 +30,33 @@ public class TernarySearchTree<T> {
             node.mid = putNode(node.mid, key, value, charIndex + 1);
         }
         return node;
+    }
+
+    public boolean contains(String key) {
+        return get(key) != null;
+    }
+
+    public T get(String key) {
+        Node retrieved = getNode(root, key, 0);
+        if (retrieved == null) {
+            return null;
+        }
+        return (T) retrieved.value;
+    }
+
+    private Node getNode(Node node, String key, int charIndex) {
+        if (node == null) {
+            return null;
+        }
+        char c = key.charAt(charIndex);
+        if (c < node.c) {
+            return getNode(node.left, key, charIndex + 1);
+        } else if (c > node.c) {
+            return getNode(node.right, key, charIndex + 1);
+        } else if (charIndex == key.length() - 1) { // last char
+            return node;
+        } else {
+            return getNode(node.mid, key, charIndex + 1);
+        }
     }
 }
