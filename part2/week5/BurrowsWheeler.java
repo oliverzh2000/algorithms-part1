2129868628f73b2477e3ad19e8d8a91ea35831e7
Oliver Zhang <oliverzh2000@gmail.com>
2019-10-18 00:59:33 -0400

    Finish inverse burrows-wheeler transform

--- part2/week5/BurrowsWheeler.java
+++ part2/week5/BurrowsWheeler.java
@@ -1,7 +1,11 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
+import java.util.Arrays;
+
 public class BurrowsWheeler {
+    private static final int R = 256;
+
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
     public static void transform() {
@@ -25,7 +29,38 @@ public class BurrowsWheeler {
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
+        int first = BinaryStdIn.readInt();
+        char[] lastCol = BinaryStdIn.readString().toCharArray();
+        char[] firstCol = lastCol.clone();
 
+//        int first = 3;
+//        char[] lastCol = {'A', 'R', 'D', '!', 'R', 'C', 'A', 'A', 'A', 'A', 'B', 'B'};
+//        char[] firstCol = {'!', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'D', 'R', 'R'};
+
+        Arrays.sort(firstCol);
+
+        int[] next = new int[firstCol.length];
+        int[] base = new int[R];
+        Arrays.fill(next, -1);
+        for (int i = 0; i < firstCol.length; i++) {
+            // find smallest j where firstCol[i] == lastCol[j].
+            for (int j = base[firstCol[i]]; j < lastCol.length; j++) {
+                if (firstCol[i] == lastCol[j]) {
+                    // TODO: keep track of chars to skip, for critical optimization.
+                    next[i] = j;
+                    base[firstCol[i]] = j + 1;
+                    break;
+                }
+            }
+        }
+
+        // Apply inverse transform from the known next[] array.
+        int currentIndex = first;
+        for (int i = 0; i < firstCol.length; i++) {
+            BinaryStdOut.write(firstCol[currentIndex], 8);
+            currentIndex = next[currentIndex];
+        }
+        BinaryStdOut.close();
     }
 
     private static char charAt(String s, int suffixOffset, int i) {
@@ -41,5 +76,4 @@ public class BurrowsWheeler {
             inverseTransform();
         }
     }
-
 }
